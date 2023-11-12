9e4c365f9e827ac121bf7794476c35284781f623
tojaroslaw <tobymac@umich.edu>
2023-11-11 23:36:36 -0500

    Add MapContains function

--- utls.go
+++ utls.go
@@ -4,3 +4,10 @@ package utls
 func ToPtr[T any](item T) *T {
 	return &item
 }
+
+// MapContains takes in a map and an item to check for as a key in that map. If the item is a key in the map, it returns
+// true; otherwise it returns false.
+func MapContains[T comparable, U any](m map[T]U, item T) bool {
+	_, ok := m[item]
+	return ok
+}
--- utls_test.go
+++ utls_test.go
@@ -5,23 +5,25 @@ import (
 	"testing"
 )
 
-func TestToPtr(t *testing.T) {
-	type toyStruct struct {
-		i      int
-		iPtr   *int
-		s      string
-		sPtr   *string
-		b      bool
-		bPtr   *bool
-		slc    []bool
-		slcPtr *[]bool
-	}
+// TEST FIXTURES
 
-	intExample := 5
-	strExample := "test"
-	boolExample := true
-	sliceExample := []bool{true, false, true}
-	structExample := toyStruct{
+type toyStruct struct {
+	i      int
+	iPtr   *int
+	s      string
+	sPtr   *string
+	b      bool
+	bPtr   *bool
+	slc    []bool
+	slcPtr *[]bool
+}
+
+func sliceExample() []bool {
+	return []bool{true, false, true}
+}
+
+func structExample() toyStruct {
+	return toyStruct{
 		i:      5,
 		iPtr:   ToPtr(5),
 		s:      "test",
@@ -31,6 +33,16 @@ func TestToPtr(t *testing.T) {
 		slc:    []bool{true, false, true},
 		slcPtr: ToPtr([]bool{true, false, true}),
 	}
+}
+
+// TESTS
+
+func TestToPtr(t *testing.T) {
+	intExample := 5
+	strExample := "test"
+	boolExample := true
+	sliceExample := sliceExample()
+	structExample := structExample()
 
 	testCases := []struct {
 		name string
@@ -125,6 +137,176 @@ func TestToPtr(t *testing.T) {
 				structPtrItem := tc.item.(*toyStruct)
 				structPtrPtr := ToPtr(structPtrItem)
 				require.Equal(t, structPtrItem, *structPtrPtr)
+			default:
+				t.Errorf("unexpected type: %T", tc.item)
+			}
+		})
+	}
+}
+
+func TestMapContains(t *testing.T) {
+	intExample := 5
+	intExample2 := 2
+	strExample := "test"
+	strExample2 := "test2"
+	boolExample := true
+	boolExample2 := false
+
+	testCases := []struct {
+		name string
+		m    map[any]any
+		item any
+		ok   bool
+	}{
+		{
+			name: "integer",
+			m: map[any]any{
+				intExample: "test",
+			},
+			item: intExample,
+			ok:   true,
+		},
+		{
+			name: "integer pointer",
+			m: map[any]any{
+				&intExample: "test",
+			},
+			item: &intExample,
+			ok:   true,
+		},
+		{
+			name: "string",
+			m: map[any]any{
+				strExample: 5,
+			},
+			item: strExample,
+			ok:   true,
+		},
+		{
+			name: "string pointer",
+			m: map[any]any{
+				&strExample: 5,
+			},
+			item: &strExample,
+			ok:   true,
+		},
+		{
+			name: "boolean",
+			m: map[any]any{
+				boolExample: 5,
+			},
+			item: boolExample,
+			ok:   true,
+		},
+		{
+			name: "boolean pointer",
+			m: map[any]any{
+				&boolExample: 5,
+			},
+			item: &boolExample,
+			ok:   true,
+		},
+		{
+			name: "integer not in map",
+			m: map[any]any{
+				intExample: "test",
+			},
+			item: intExample2,
+			ok:   false,
+		},
+		{
+			name: "integer pointer not in map",
+			m: map[any]any{
+				&intExample: "test",
+			},
+			item: &intExample2,
+			ok:   false,
+		},
+		{
+			name: "string not in map",
+			m: map[any]any{
+				strExample: 5,
+			},
+			item: strExample2,
+			ok:   false,
+		},
+		{
+			name: "string pointer not in map",
+			m: map[any]any{
+				&strExample: 5,
+			},
+			item: &strExample2,
+			ok:   false,
+		},
+		{
+			name: "boolean not in map",
+			m: map[any]any{
+				boolExample: 5,
+			},
+			item: boolExample2,
+			ok:   false,
+		},
+		{
+			name: "boolean pointer not in map",
+			m: map[any]any{
+				&boolExample: 5,
+			},
+			item: &boolExample2,
+			ok:   false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			switch tc.item.(type) {
+			case int:
+				intItem := tc.item.(int)
+				m := map[int]any{}
+				for k, v := range tc.m {
+					m[k.(int)] = v
+				}
+				intOk := MapContains(m, intItem)
+				require.Equal(t, tc.ok, intOk)
+			case *int:
+				intPtrItem := tc.item.(*int)
+				m := map[*int]any{}
+				for k, v := range tc.m {
+					m[k.(*int)] = v
+				}
+				intPtrOk := MapContains(m, intPtrItem)
+				require.Equal(t, tc.ok, intPtrOk)
+			case string:
+				stringItem := tc.item.(string)
+				m := map[string]any{}
+				for k, v := range tc.m {
+					m[k.(string)] = v
+				}
+				stringOk := MapContains(m, stringItem)
+				require.Equal(t, tc.ok, stringOk)
+			case *string:
+				stringPtrItem := tc.item.(*string)
+				m := map[*string]any{}
+				for k, v := range tc.m {
+					m[k.(*string)] = v
+				}
+				stringPtrOk := MapContains(m, stringPtrItem)
+				require.Equal(t, tc.ok, stringPtrOk)
+			case bool:
+				boolItem := tc.item.(bool)
+				m := map[bool]any{}
+				for k, v := range tc.m {
+					m[k.(bool)] = v
+				}
+				boolOk := MapContains(m, boolItem)
+				require.Equal(t, tc.ok, boolOk)
+			case *bool:
+				boolPtrItem := tc.item.(*bool)
+				m := map[*bool]any{}
+				for k, v := range tc.m {
+					m[k.(*bool)] = v
+				}
+				boolPtrOk := MapContains(m, boolPtrItem)
+				require.Equal(t, tc.ok, boolPtrOk)
 			default:
 				t.Errorf("unexpected type: %T", tc.item)
 			}
